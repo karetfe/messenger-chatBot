bcc8c00a8ffff57444b1f38fe3dba6185d0da885
bechir <nemlaghi.bechir@gmail.com>
2017-09-18 11:32:22 +0100

    hardcoded token :ghost:

--- app.js
+++ app.js
@@ -14,6 +14,17 @@ app.get('/', function(req, res) {
   res.status(200).send("it works");
 });
 
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === "1d875c36-9c5c-11e7-abc4-cec278b6b50a") {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }
+});
+
 var server = app.listen(process.env.OPENSHIFT_NODEJS_PORT || 3000, process.env.OPENSHIFT_NODEJS_IP || 'localhost',function () {
   console.log("Listening on port %s", server.address().port);
 });
